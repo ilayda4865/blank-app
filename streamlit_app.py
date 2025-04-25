fa6c5c48655deb6697c183e9e875b1623b645333
ilayda4865 <ilaydaoptima@gmail.com>
2025-04-25 09:29:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
+
 col1, col2 = st.columns([1, 5])
 # with col1:
 #     st.image("Yeni klasör/logo.jpg", width=100)
 
 with col2:
+    st.image("logo.jpg", width=150)
     st.title("Project OPT-0158")
 
 title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">Truck Based Laboratory Container (BSL3)</p>'
