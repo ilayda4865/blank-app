9ac41150cc082f69ee4111978c10d430308597be
ilayda4865 <ilaydaoptima@gmail.com>
2025-04-25 11:24:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 
 col1, col2 = st.columns([1, 5])
-# with col1:
-#     st.image("Yeni klasör/logo.jpg", width=100)
-st.image("logo.jpg", width=150)
+with col1:
+    st.image("logo.jpg", width=100)
 with col2:
     st.title("Project OPT-0158")
 
